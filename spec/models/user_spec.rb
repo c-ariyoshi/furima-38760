2c485eed15d23ec4123e6715658a81ab68eac9da
c-ariyoshi <121472025+c-ariyoshi@users.noreply.github.com>
2023-04-03 19:13:25 +0900

    ユーザー管理機能

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -49,12 +49,12 @@ RSpec.describe User, type: :model do
       it "passwordが英語のみでは登録できない" do
         @user.password = 'abcdef'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password", "Password 半角英数を両方含む必要があります")
+        expect(@user.errors.full_messages).to include("Password 半角英数を両方含む必要があります")
       end
       it "passwordが数字のみでは登録できない" do
         @user.password = '123456'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password", "Password 半角英数を両方含む必要があります")
+        expect(@user.errors.full_messages).to include("Password 半角英数を両方含む必要があります")
       end
       it "passwordとpassword_confirmationが不一致では登録できない" do
         @user.password = '123456'
