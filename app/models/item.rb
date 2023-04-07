e52d1ae8abe10ffa016a47f273b5afd75d24921d
c-ariyoshi <121472025+c-ariyoshi@users.noreply.github.com>
2023-04-07 18:04:43 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :prefecture
+  belongs_to :shipping_date
+  belongs_to :shipping_fee
+
 end
