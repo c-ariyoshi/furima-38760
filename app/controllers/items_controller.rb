af7e9ffbd6a0acf640b61c47c09c6ea0c0d54a75
c-ariyoshi <121472025+c-ariyoshi@users.noreply.github.com>
2023-04-08 11:18:26 +0900

    商品出品ページ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,36 @@
 class ItemsController < ApplicationController
-  def index
+
+  def edit
   end
+
+  def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+ 
+
+  private
+
+ def item_params
+   params.require(:item).permit(:name, :price, :description, :category_id, :item_condition_id,
+    :prefecture_id, :shipping_fee_id, :shipping_date_id, :image).merge(user_id: current_user.id)
+ end
+
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -7,5 +8,16 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_date
   belongs_to :shipping_fee
+
+  validates :name, :description, :category_id, :item_condition_id,
+   :prefecture_id, :shipping_fee_id, :shipping_date_id, :user_id, presence: true, unless: :was_attached?
+  validates :price, presence: true, unless: :was_attached?, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+    format: { with: /\A[0-9]+\z/, message: '半角数字で入力して下さい'  }
+  validates :category_id, :item_condition_id, :prefecture_id, :shipping_fee_id,
+  :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank" } 
+
+  def was_attached?
+    self.image.attached?
+  end
 
 end
